8284f9ff1f9bebbbe36977b9f3141e863c01c25d
timmyreilly <timreilly@live.com>
2015-12-30 17:20:05 -0800

    removed textbook stuff

--- Textbookstuff/Chapter07/survey_project/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-
-current_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-
-sys.path.insert(0, parent_dir)
